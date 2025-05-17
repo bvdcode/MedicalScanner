6288858bb1b65327480bfab5112e9b672c4b9873
Vadim Belov <github@belov.us>
2025-05-17 08:00:43 -0700

    Refactor Bluetooth scanning functionality
    Updated MainPage.xaml to rename UI elements and change event handlers. Introduced a new list to track discovered devices in MainPage.xaml.cs, replaced the click handler with enhanced scanning logic, and added permission checks for location and Wi-Fi. Implemented a method to visually represent signal strength.

--- Sources/MainPage.xaml.cs
+++ Sources/MainPage.xaml.cs
@@ -1,24 +1,48 @@
 ﻿using Plugin.BLE;
 using Plugin.BLE.Abstractions.Contracts;
 using Plugin.BLE.Abstractions.EventArgs;
+using System.Text;
 
 namespace MedicalScanner
 {
     public partial class MainPage : ContentPage
     {
         private readonly IAdapter _adapter;
+        private readonly List<IDevice> _devices;
 
         public MainPage()
         {
             InitializeComponent();
-
+            _devices = [];
             _adapter = CrossBluetoothLE.Current.Adapter;
             _adapter.DeviceDiscovered += OnDeviceDiscovered;
         }
 
-        private async void OnCounterClicked(object? sender, EventArgs e)
+        private async void OnToggleBtnClicked(object? sender, EventArgs e)
         {
-            OutputLabel.Text = "Scanning for Low-Energy devices...";
+            if (_adapter.IsScanning)
+            {
+                await _adapter.StopScanningForDevicesAsync();
+                _devices.Clear();
+            }
+            if (!CrossBluetoothLE.Current.IsOn)
+            {
+                await DisplayAlert("Bluetooth Disabled", "Please enable Bluetooth to scan for devices.", "OK");
+                return;
+            }
+            if (_devices.Count == 0)
+            {
+                OutputLabel.Text = "Scanning for Low-Energy devices...";
+            }
+            if (!await CheckPermissionsAsync())
+            {
+                return;
+            }
+            await _adapter.StartScanningForDevicesAsync();
+        }
+
+        private async Task<bool> CheckPermissionsAsync()
+        {
             var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
             if (status != PermissionStatus.Granted)
             {
@@ -26,19 +50,69 @@ namespace MedicalScanner
                 if (status != PermissionStatus.Granted)
                 {
                     await DisplayAlert("Permission Required", "Location permission is needed for Bluetooth scanning.", "OK");
-                    return;
+                    return false;
                 }
             }
-            await _adapter.StartScanningForDevicesAsync();
+            status = await Permissions.CheckStatusAsync<Permissions.NearbyWifiDevices>();
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.NearbyWifiDevices>();
+                if (status != PermissionStatus.Granted)
+                {
+                    await DisplayAlert("Permission Required", "Bluetooth permission is needed for Bluetooth scanning.", "OK");
+                    return false;
+                }
+            }
+            return true;
         }
 
         private void OnDeviceDiscovered(object? sender, DeviceEventArgs args)
         {
-            var name = string.IsNullOrWhiteSpace(args.Device.Name) ? "[Unnamed]" : args.Device.Name;
+            if (_devices.Any(x => x.Id == args.Device.Id))
+            {
+                return;
+            }
+            _devices.Add(args.Device);
+
+            StringBuilder sb = new();
+            foreach (var device in _devices.OrderByDescending(x => x.Rssi))
+            {
+                string name = string.IsNullOrWhiteSpace(device.Name) ? "[Unnamed]" : device.Name;
+                sb.AppendLine($"{GetSignalLevel(device.Rssi)}: {name}");
+            }
+
             MainThread.BeginInvokeOnMainThread(() =>
             {
-                OutputLabel.Text += $"\n{name}";
+                OutputLabel.Text = sb.ToString();
             });
+        }
+
+        private static string GetSignalLevel(int rssi)
+        {
+            if (rssi >= -60)
+            {
+                return "◉◉◉◉◉";
+            }
+            else if (rssi >= -70)
+            {
+                return "◉◉◉◉○";
+            }
+            else if (rssi >= -80)
+            {
+                return "◉◉◉○○";
+            }
+            else if (rssi >= -90)
+            {
+                return "◉◉○○○";
+            }
+            else if (rssi >= -100)
+            {
+                return "◉○○○○";
+            }
+            else
+            {
+                return "○○○○○";
+            }
         }
     }
 }
